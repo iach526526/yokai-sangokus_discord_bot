71b88898fb2f8cbd0f5a12c9be342e83e28bd877
Each Chen <iach526526@gmail.com>
2023-04-06 17:06:47 +0800

    perf:刪除重複的函式
    已經寫過一次，不小心複製兩個

--- Cmds/game_data_catcher.py
+++ Cmds/game_data_catcher.py
@@ -52,17 +52,6 @@ def link_start(url:str):
     root = bs4.BeautifulSoup(data, "html.parser")
     return root
 
-def link_start(url: str):
-    request = req.Request(
-        url,
-        headers={
-            "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
-        })
-    with req.urlopen(request) as response:
-        data = response.read().decode("utf-8")
-    root = bs4.BeautifulSoup(data, "html.parser")
-    return root
 # 回傳經BS套件整理後的網頁
 
 
